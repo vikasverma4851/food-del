9fead493a149835c09dead732699ea6fab6f0da6
Vikas Verma <vikasverma4851@gmail.com>
2024-09-09 03:25:38 +0530

    deploy change

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config.js";
+
 //app config
 const app = express();
 const port = 4000;
@@ -12,6 +16,11 @@ app.use(cors());
 
 // db connection
 connectDB();
+
+//api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads")); //for excess using url
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
