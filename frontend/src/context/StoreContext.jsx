c134b4a46a9768ce29b2f69b220ac3b8633b1787
Vikas Verma <vikasverma4851@gmail.com>
2024-11-25 07:17:58 +0530

    localhost path change to live path

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-del-2-943z.onrender.com/";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
   const addToCart = async (itemId) => {
