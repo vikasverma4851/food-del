a4f9d76b1a517b2f3821057ab088ce3e8e3ee67d
Vikas Verma <vikasverma4851@gmail.com>
2024-11-25 07:53:50 +0530

    localhost path changes

--- backend/server.js
+++ backend/server.js
@@ -24,12 +24,12 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads")); //for excess using url
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order",orderRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
 app.listen(port, () => {
-  console.log(`Server Started on http://localhost:${port}`);
+  console.log(`Server Started on https://food-del-2-943z.onrender.com:${port}`);
 });
